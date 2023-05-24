12647fca72f9a1ec471ac94a67d3bae138f30f6d
Basit <basit>
2023-05-24 22:01:18 +0530

    Finished the Version Alpha of the website 24/05/2023

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let Container1 = document.querySelector(".subContainer1")
 let Container2 = document.querySelector(".subContainer2")
+let arrowLeft = document.querySelector(".arrowLeft")
+let arrowRight = document.querySelector(".arrowRight")
+let slider = document.querySelector(".slider")
+let circle1 = document.querySelector(".circle1")
+let circle2 = document.querySelector(".circle2")
+
 Container1.addEventListener("click", () => {
     Container1.style.backgroundColor = "rgb(236, 236, 236)"
     Container2.style.backgroundColor = "transparent"
@@ -10,5 +16,34 @@ Container2.addEventListener("click", () => {
     Container2.style.backgroundColor = "rgb(236, 236, 236)"
     Container1.style.backgroundColor = "transparent"
     Container2.style.transition = "0.5s ease-in-out"
+
+})
+
+arrowRight.addEventListener("click", () => {
+
+    slider.style.transform = "translateX(-49vh)"
+    circle1.style.backgroundColor = "rgb(193, 233, 250)"
+    circle2.style.backgroundColor = "rgb(51, 132, 167)"
+
+
+})
+
+arrowLeft.addEventListener("click", () => {
+
+    slider.style.transform = "translateX(0vh)"
+    circle2.style.backgroundColor = "rgb(193, 233, 250)"
+    circle1.style.backgroundColor = "rgb(51, 132, 167)"
+
+})
+
+circle1.addEventListener("click", () => {
+    slider.style.transform = "translateX(0vh)"
+    circle2.style.backgroundColor = "rgb(193, 233, 250)"
+    circle1.style.backgroundColor = "rgb(51, 132, 167)"
+})
+circle2.addEventListener("click", () => {
+    slider.style.transform = "translateX(-49vh)"
+    circle1.style.backgroundColor = "rgb(193, 233, 250)"
+    circle2.style.backgroundColor = "rgb(51, 132, 167)"
 
 })
